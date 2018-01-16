a16982ac4c44cf06d05daa7581dbf235bf4916fc
mjkovac <mattjkovac@gmail.com>
2018-01-16 14:01:26 -0500

    Node
    Intro

---
+++ nodeIntro/js/main.js
@@ -0,0 +1,9 @@
+$("section").animate({ top:'25%' }, 1000);
+
+$("section").animate({ left:'25%' }, 1000);
+
+$("section").animate({ top:'85%' }, 1000);
+
+$("section").animate({ left:'90%' }, 1000);
+
+$("section").animate({ top:'92%' }, 1000);
